ff3e1f95091d2b1d0cd6542af2ff36ec67147ed4
Isaac Gutekunst <isaac@isaac.cc>
2019-06-12 15:51:58 -0700

    Updated Basic example

--- src/arduino/BinaryCodec/raw/BinaryCodec/examples/Basic/Basic.ino
+++ src/arduino/BinaryCodec/raw/BinaryCodec/examples/Basic/Basic.ino
@@ -1,15 +1,16 @@
 #include "BinaryCodec.h"
-#include "serial_hal."
+#include "serial_hal.h"
 
 static union ConcentratorPacket latestRxPacket;
 static uint8_t tx_packet_buffer[30];
 void setup() {
-    SPI.begin(SPI_MODE_MASTER);
+
 }
 
 void loop() {
     size_t bytes_encoded = 0;
     packet_encode_to_byte_array(tx_packet_buffer, &latestRxPacket, sizeof(tx_packet_buffer), &bytes_encoded);
+    SerialPort* serial;
     serial_send_bytes(serial, tx_packet_buffer, bytes_encoded);
 
 }
